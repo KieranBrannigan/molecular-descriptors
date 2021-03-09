a719596a64f3afe23f9e700506b63bc8d36f24ea
Kieran Brannigan <kieran.t.brannigan@gmail.com>
2021-03-09 01:12:11 +0000

    Add a structural_distance fun

--- y4_python/python_modules/structural_similarity.py
+++ y4_python/python_modules/structural_similarity.py
@@ -1,7 +1,9 @@
 from rdkit import DataStructs
+from rdkit.DataStructs.cDataStructs import ExplicitBitVect
+
 from .util import fingerprint_from_smiles
 
-def similarity_between_two_molecules(s1: str, s2: str, fingerprint_type: int):
+def similarity_between_two_smiles(s1: str, s2: str, fingerprint_type: int):
     """
     Helper function for quickly printing similarity between two molecules from SMILES
 
@@ -12,3 +14,7 @@ def similarity_between_two_molecules(s1: str, s2: str, fingerprint_type: int):
     fp2 = fingerprint_from_smiles(s2, fingerprint_type=fingerprint_type)
 
     return DataStructs.FingerprintSimilarity(fp1, fp2 ,metric=DataStructs.TanimotoSimilarity)
+
+def structural_distance(fp1: ExplicitBitVect, fp2: ExplicitBitVect):
+    "Return 1 - tanimotoSimilarity between fp1 and fp2"
+    return 1 - DataStructs.FingerprintSimilarity(fp1, fp2 ,metric=DataStructs.TanimotoSimilarity)
