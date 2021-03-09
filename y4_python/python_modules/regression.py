583e3d0b08a5e51cf258bd0de8e9fcf4c9fd0dc9
Kieran Brannigan <kieran.t.brannigan@gmail.com>
2021-03-09 01:11:58 +0000

    Add doc comment

--- y4_python/python_modules/regression.py
+++ y4_python/python_modules/regression.py
@@ -13,6 +13,15 @@ y = blyp_energies
 slope, intercept, r_value, p_value, std_err = linregress(x,y)
 
 def distance_from_regress(x, y):
+    """
+    y' is y_value from linear regression
+    y is real y_value
+
+    return y' - y
+
+    To improve given y value to be on regression line, then
+    do: distance_from_regress(x,y) + y 
+    """
     y_regress = slope*x + intercept
     dist = y_regress - y
     return dist
