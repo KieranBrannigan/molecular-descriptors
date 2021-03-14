4921027f941cf28772d351d11d9b258ab5b3fe65
Kieran Brannigan <kieran.t.brannigan@gmail.com>
2021-03-14 03:20:13 +0000

    Generalize regression to multiple DBs

--- y4_python/python_modules/regression.py
+++ y4_python/python_modules/regression.py
@@ -2,26 +2,28 @@ from scipy.stats import linregress
 
 from .database import DB
 
-db = DB()
+class MyRegression:
+    def __init__(self, db):
+        self.db = db
+        pm7_energies = db.get_pm7_energies()
+        blyp_energies = db.get_blyp_energies()
 
-pm7_energies = db.get_pm7_energies()
-blyp_energies = db.get_blyp_energies()
+        x = pm7_energies
+        y = blyp_energies
 
-x = pm7_energies
-y = blyp_energies
+        self.slope, self.intercept, self.r_value, self.p_value, self.std_err = linregress(x,y)
 
-slope, intercept, r_value, p_value, std_err = linregress(x,y)
+    
+    def distance_from_regress(self, x, y):
+        """
+        y' is y_value from linear regression
+        y is real y_value
 
-def distance_from_regress(x, y):
-    """
-    y' is y_value from linear regression
-    y is real y_value
+        return y' - y
 
-    return y' - y
-
-    To improve given y value to be on regression line, then
-    do: distance_from_regress(x,y) + y 
-    """
-    y_regress = slope*x + intercept
-    dist = y_regress - y
-    return dist
+        To improve given y value to be on regression line, then
+        do: distance_from_regress(x,y) + y 
+        """
+        y_regress = self.slope*x + self.intercept
+        dist = y_regress - y
+        return dist
