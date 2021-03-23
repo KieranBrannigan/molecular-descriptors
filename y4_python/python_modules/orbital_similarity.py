17c2341cc2f4c96b1cc206eee781a698c0943e12
Kieran Brannigan <kieran.t.brannigan@gmail.com>
2021-03-23 12:28:34 +0000

    Impl. percentHetero and normalize inertia_diff

--- y4_python/python_modules/orbital_similarity.py
+++ y4_python/python_modules/orbital_similarity.py
@@ -11,12 +11,21 @@ def inertia_difference(moments1: Union[Sequence[float], Tuple[float, float, floa
     """
     Calculate some difference between (calculated) two molecular orbitals
     based on their moment of inertia.
+
+    New Distance: ( (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2) / (x1*x2+y1*y2+z1*z2)
     """
     m1 = sorted(moments1)
     m2 = sorted(moments2)
-    return sum(
+    length_squared = sum(
         [(m1[idx] - m2[idx])**2 for idx in range(len(moments1))]
-    )**0.5
+    )
+    dot_squared = sum([m1[idx] * m2[idx] for idx in range(len(moments1))]) ** 2
+
+    ### a . b = |a||b|cos(x)
+    ### 1/cos(x) = |a||b| / (a.b)
+    ### ( |a||b| / (a.b) )^2 = 1/cos^2(x) , which ranges between 0->1
+
+    return length_squared / dot_squared
 
 
 def IPR_difference(mo1: SerializedMolecularOrbital, mo2: SerializedMolecularOrbital):
@@ -29,12 +38,22 @@ def IPR_difference(mo1: SerializedMolecularOrbital, mo2: SerializedMolecularOrbi
 def percent_heteroatom_difference(mo1: SerializedMolecularOrbital, mo2: SerializedMolecularOrbital, atom_symbol: str):
     """
     Calculate some difference between two (calculated) molecular orbitals
-    based on there percent weight on specified heteroatom.
+    based on there percent weight on heteroatoms N, S and O.
     """
-    return 0
+    symbol_key_map = {
+        "N": "percent_on_N"
+        , "O": "percent_on_O"
+        , "S": "percent_on_S"
+        , "P": "percent_on_P"
+    }
+
+    key = symbol_key_map[atom_symbol]
+    diff = abs( mo1[key] - mo2[key] )
+    
+    return diff
 
 
-def orbital_distance(mo1: SerializedMolecularOrbital, mo2: SerializedMolecularOrbital, inertia_coeff:float=1., IPR_coeff:float=1, O_coeff:float=1, N_coeff:float=1):
+def orbital_distance(mo1: SerializedMolecularOrbital, mo2: SerializedMolecularOrbital, inertia_coeff:float=1., IPR_coeff:float=1, O_coeff:float=1, N_coeff:float=1, S_coeff:float=1):
     """
     Compute the Distance between 2 (calculated) molecular orbitals.
 
@@ -43,16 +62,22 @@ def orbital_distance(mo1: SerializedMolecularOrbital, mo2: SerializedMolecularOr
 
     I suspect the most important will be O,N percent, then moment of inertia, then IPR.
 
-    TODO: should the distance should be normalised to between 0 and 1?
-
     """
 
     inertia_diff = inertia_difference(mo1["principal_moments"], mo2["principal_moments"])
     IPR_diff = IPR_difference(mo1, mo2)
-    O_diff = percent_heteroatom_difference(mo1, mo2, "O")
-    N_diff = percent_heteroatom_difference(mo1, mo2, "N")
 
-    distance = inertia_coeff * inertia_diff + IPR_coeff * IPR_diff + O_coeff * O_diff + N_coeff * N_diff
+    heteroatom_coeff_sum = O_coeff + N_coeff + S_coeff
+    if heteroatom_coeff_sum == 0:
+        heteroatom_diff = 0
+    else:
+        O_diff = percent_heteroatom_difference(mo1, mo2, "O")
+        N_diff = percent_heteroatom_difference(mo1, mo2, "N")
+        S_diff = percent_heteroatom_difference(mo1, mo2, "S")
+        heteroatom_diff = ( O_coeff * O_diff + N_coeff * N_diff + S_diff * S_coeff )  / heteroatom_coeff_sum
+
+
+    distance = inertia_coeff * inertia_diff + IPR_coeff * IPR_diff + heteroatom_diff
     
     return distance
 
